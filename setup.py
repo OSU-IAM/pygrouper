a7857b5725eb991d59414a573ea29eafceb0c71a
morgan <morgan@oregonstate.edu>
2019-10-23 13:04:31 -0700

    Add get_members_pit() method

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygrouper",
-    version="1.0.0",
+    version="1.0.1",
     author="stacybrock",
     description="Python module for working with Grouper",
     long_description=long_description,
